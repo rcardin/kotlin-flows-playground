1d28e4e97a62be736b87642bf1b22c2ea4477243
rcardin <riccardo.cardin@gmail.com>
2024-02-28 21:47:56 +0100

    Added some examples of Flow builders

--- src/main/kotlin/in/rcard/kotlin/flows/Main.kt
+++ src/main/kotlin/in/rcard/kotlin/flows/Main.kt
@@ -4,6 +4,9 @@ import `in`.rcard.kotlin.flows.Model.Actor
 import `in`.rcard.kotlin.flows.Model.FirstName
 import `in`.rcard.kotlin.flows.Model.Id
 import `in`.rcard.kotlin.flows.Model.LastName
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flowOf
 
 object Model {
     @JvmInline value class Id(val id: Int)
@@ -15,29 +18,50 @@ object Model {
     data class Actor(val id: Id, val firstName: FirstName, val lastName: LastName)
 }
 
-object Data {
-    // Zack Snyder's Justice League
-    val henryCavill = Actor(Id(1), FirstName("Henry"), LastName("Cavill"))
-    val galGodot: Actor = Actor(Id(1), FirstName("Gal"), LastName("Godot"))
-    val ezraMiller: Actor = Actor(Id(2), FirstName("Ezra"), LastName("Miller"))
-    val benFisher: Actor = Actor(Id(3), FirstName("Ben"), LastName("Fisher"))
-    val rayHardy: Actor = Actor(Id(4), FirstName("Ray"), LastName("Hardy"))
-    val jasonMomoa: Actor = Actor(Id(5), FirstName("Jason"), LastName("Momoa"))
+// Zack Snyder's Justice League
+val henryCavill = Actor(Id(1), FirstName("Henry"), LastName("Cavill"))
+val galGodot: Actor = Actor(Id(1), FirstName("Gal"), LastName("Godot"))
+val ezraMiller: Actor = Actor(Id(2), FirstName("Ezra"), LastName("Miller"))
+val benFisher: Actor = Actor(Id(3), FirstName("Ben"), LastName("Fisher"))
+val rayHardy: Actor = Actor(Id(4), FirstName("Ray"), LastName("Hardy"))
+val jasonMomoa: Actor = Actor(Id(5), FirstName("Jason"), LastName("Momoa"))
 
-    // The Avengers
-    val robertDowneyJr: Actor = Actor(Id(6), FirstName("Robert"), LastName("Downey Jr."))
-    val chrisEvans: Actor = Actor(Id(7), FirstName("Chris"), LastName("Evans"))
-    val markRuffalo: Actor = Actor(Id(8), FirstName("Mark"), LastName("Ruffalo"))
-    val chrisHemsworth: Actor = Actor(Id(9), FirstName("Chris"), LastName("Hemsworth"))
-    val scarlettJohansson: Actor = Actor(Id(10), FirstName("Scarlett"), LastName("Johansson"))
-    val jeremyRenner: Actor = Actor(Id(11), FirstName("Jeremy"), LastName("Renner"))
+// The Avengers
+val robertDowneyJr: Actor = Actor(Id(6), FirstName("Robert"), LastName("Downey Jr."))
+val chrisEvans: Actor = Actor(Id(7), FirstName("Chris"), LastName("Evans"))
+val markRuffalo: Actor = Actor(Id(8), FirstName("Mark"), LastName("Ruffalo"))
+val chrisHemsworth: Actor = Actor(Id(9), FirstName("Chris"), LastName("Hemsworth"))
+val scarlettJohansson: Actor = Actor(Id(10), FirstName("Scarlett"), LastName("Johansson"))
+val jeremyRenner: Actor = Actor(Id(11), FirstName("Jeremy"), LastName("Renner"))
 
-    // Spider Man
-    val tomHolland: Actor = Actor(Id(12), FirstName("Tom"), LastName("Holland"))
-    val tobeyMaguire: Actor = Actor(Id(13), FirstName("Tobey"), LastName("Maguire"))
-    val andrewGarfield: Actor = Actor(Id(14), FirstName("Andrew"), LastName("Garfield"))
-}
+// Spider Man
+val tomHolland: Actor = Actor(Id(12), FirstName("Tom"), LastName("Holland"))
+val tobeyMaguire: Actor = Actor(Id(13), FirstName("Tobey"), LastName("Maguire"))
+val andrewGarfield: Actor = Actor(Id(14), FirstName("Andrew"), LastName("Garfield"))
 
 fun main() {
-    println("Hello, world!")
+    val zackSnyderJusticeLeague: Flow<Actor> =
+        flowOf(
+            henryCavill,
+            galGodot,
+            ezraMiller,
+            benFisher,
+            rayHardy,
+            jasonMomoa,
+        )
+
+    val avengers: Flow<Actor> =
+        listOf(
+            robertDowneyJr,
+            chrisEvans,
+            markRuffalo,
+            chrisHemsworth,
+            scarlettJohansson,
+            jeremyRenner,
+        )
+            .asFlow()
+
+    val theMostRecentSpiderManFun: () -> Actor = { tomHolland }
+
+    val theMostRecentSpiderMan: Flow<Actor> = theMostRecentSpiderManFun.asFlow()
 }
